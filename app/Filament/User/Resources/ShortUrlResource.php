fa1c306c1f6c3b5035871a27dd04dbee2c16717b
Putra Prima Arhandi <putraprima@gmail.com>
2024-07-14 09:07:31 +0900

    Feat : Short Url Resource
    Add custom create funtion using short url library.

---
+++ app/Filament/User/Resources/ShortUrlResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\User\Resources;
+
+use App\Filament\User\Resources\ShortUrlResource\Pages;
+use App\Filament\User\Resources\ShortUrlResource\RelationManagers;
+use AshAllenDesign\ShortURL\Models\ShortURL;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ShortUrlResource extends Resource
+{
+    protected static ?string $model = ShortURL::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //add text input for destination_url
+                TextInput::make('destination_url')
+                    ->label('Destination URL')
+                    ->required()
+                    ->placeholder('https://example.com'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //add column for destination_url
+                TextColumn::make('destination_url')
+                    ->searchable()
+                    ->sortable(),
+                //add column for url_key
+                TextColumn::make('url_key')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListShortUrls::route('/'),
+            'create' => Pages\CreateShortUrl::route('/create'),
+            'edit' => Pages\EditShortUrl::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/User/Resources/ShortUrlResource/Pages/CreateShortUrl.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\User\Resources\ShortUrlResource\Pages;
+
+use App\Filament\User\Resources\ShortUrlResource;
+use AshAllenDesign\ShortURL\Facades\ShortURL as FacadesShortURL;
+use AshAllenDesign\ShortURL\Models\ShortURL;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateShortUrl extends CreateRecord
+{
+    protected static string $resource = ShortUrlResource::class;
+
+    protected function handleRecordCreation(array $data): ShortURL
+    {
+        return FacadesShortURL::destinationUrl($data['destination_url'])->make();
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
