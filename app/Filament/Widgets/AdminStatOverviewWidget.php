f1e6f438553235d7f1278001bdcea123045d6e14
Putra Prima Arhandi <putraprima@gmail.com>
2024-07-19 07:15:30 +0900

    feat: Add total user, total links, and total visit stats to AdminStatOverviewWidget
    This commit updates the AdminStatOverviewWidget to display the total number of users, total number of links created, and total number of visits. These stats provide valuable insights into the usage of the application and help improve the analytics feature. The stats are retrieved from the respective database tables using Laravel's DB facade.

--- app/Filament/Widgets/AdminStatOverviewWidget.php
+++ app/Filament/Widgets/AdminStatOverviewWidget.php
@@ -4,24 +4,22 @@ namespace App\Filament\Widgets;
 
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\DB;
 
 class AdminStatOverviewWidget extends BaseWidget
 {
     protected function getStats(): array
     {
+        //count total user
+        $totalUser = DB::table('users')->count();
+        //count total links created
+        $totalLinks = DB::table('short_urls')->count();
+        //count total visit
+        $totalVisit = DB::table('short_url_visits')->count();
         return [
-            Stat::make('Unique views', '192.1k')
-                ->description('32k increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success'),
-            Stat::make('Bounce rate', '21%')
-                ->description('7% increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-down')
-                ->color('danger'),
-            Stat::make('Average time on page', '3:12')
-                ->description('3% increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success'),
+            Stat::make('Unique User', $totalUser),
+            Stat::make('Total Links', $totalLinks),
+            Stat::make('Total Visit', $totalVisit),
         ];
     }
 }
---
+++ resources/views/widget/admin/stats.blade.php
@@ -0,0 +1,3 @@
+<div>
+    @livewire(\App\Filament\User\Resources\Widgets\AdminStatsOverviewWidget::class)
+</div>
